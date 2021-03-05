5698a885e362a7446442d46ec701efd4dda71dc5
Daniel Lee Beasley <dannyleebeasley@gmail.com>
2021-03-05 17:58:40 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -4,9 +4,9 @@ end
 
 def monopoly_with_second_tier
   base_hash[:railroads] = {}
-  base_hash[:railroads] << pieces: 4
-  base_hash[:railroads] << :rent_in_dollars => {}
-  base_hash[:railroads] << :names => {}
+  base_hash[:railroads] << :pieces => 4
+  base_hash[:railroads] << :rent_in_dollars = {}
+  base_hash[:railroads] << :names = {}
 end
 
 def monopoly_with_third_tier
